2db6e3cee1d855e4b0669d0716eeeb5dd5680930
lewyonq <jakub.lewandowski98@gmail.com>
2022-12-22 18:30:33 +0100

    278. First Bad Version works only on leetcode

---
+++ src/FirstBadVersion278.java
@@ -0,0 +1,30 @@
+public class FirstBadVersion278 {
+    public static void main(String[] args) {
+    }
+    // works on leetcode
+    //https://leetcode.com/problems/first-bad-version/description/?envType=study-plan&id=algorithm-i
+
+    public static int firstBadVersion(int n, int bad) {
+        /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+//        public class Solution extends VersionControl {
+//            public int firstBadVersion(int n) {
+//                int low = 1;
+//                int high = n;
+//
+//                while(low < high) {
+//                    int mid = (low + high) >>> 1;
+//
+//                    if(isBadVersion(mid)) {
+//                        high = mid;
+//                    } else {
+//                        low = mid + 1;
+//                    }
+//                }
+//
+//                return low;
+//            }
+//        }
+    }
+}
