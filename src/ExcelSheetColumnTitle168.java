f5be921e0b4e08f6bedb640d602abbd0ddd2f82e
lewyonq <jakub.lewandowski98@gmail.com>
2023-02-02 17:52:28 +0100

    168. little fix

--- src/ExcelSheetColumnTitle168.java
+++ src/ExcelSheetColumnTitle168.java
@@ -10,18 +10,13 @@ public class ExcelSheetColumnTitle168 {
         // 10-base system to 26-base system
         StringBuilder sb = new StringBuilder();
 
-        while (columnNumber > 26) {
+        while (columnNumber > 0) {
             int r = columnNumber % 26;
-
-            if (r == 0) {
-                r = 26;
-            }
+            if(r == 0) r = 26;
             sb.append((char) (r + 64));
             columnNumber -= r;
             columnNumber /= 26;
         }
-        sb.append((char)(columnNumber + 64));
-
         return sb.reverse().toString();
     }
 }
