37961cf6d1c3c3e879651964b69421a8efcd9688
lewyonq <jakub.lewandowski98@gmail.com>
2023-02-06 10:12:42 +0000

    fixx

---
+++ src/MaximumDepthOfBinaryTree104.java
@@ -0,0 +1,22 @@
+public class MaximumDepthOfBinaryTree104 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root));
+    }
+
+    static int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left,right) + 1;
+
+    }
+}
